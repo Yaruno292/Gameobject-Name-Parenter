e03a4c89da9e4122f53708190dc159678417597b
Yaruno <yarunoyo@outlook.com>
2021-05-07 15:41:06 +0200

    Editor update
    Made a button to activate script in editor mode

---
+++ Script/NameReParenterEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NameReParenter))]
+public class NameReParenterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        NameReParenter reParenter = (NameReParenter)target;
+        if (GUILayout.Button("Combine"))
+        {
+            reParenter.ReParent();
+        }
+    }
+}
